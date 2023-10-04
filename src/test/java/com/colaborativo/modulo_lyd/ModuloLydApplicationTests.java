741ff93891e684b3e8a38c3c52547c941528ce7e
Alejandro Escalante <alexalante2@gmail.com>
2023-10-04 06:21:07 -0600

    Inicializar proyecto Spring Boot
    Este commit marca la inicialización del proyecto Spring Boot. Se incluyen las configuraciones iniciales del proyecto, las dependencias del proyecto, la estructura de directorios y la primera versión de la migración de la base de datos.
    - Configuración de Spring Boot.
    - Dependencias y plugins del proyecto.
    - Estructura de directorios básica.
    - Primera versión de la migración de la base de datos.

---
+++ src/test/java/com/colaborativo/modulo_lyd/ModuloLydApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colaborativo.modulo_lyd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloLydApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
