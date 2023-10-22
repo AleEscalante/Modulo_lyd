dbd04e977feb60d5e90c404550d3910e98ec21a1
Tyrone Pilay <tyronepilay11@gmail.com>
2023-10-22 11:07:31 -0500

    feat(feature-conductores:Swagger):Corrección de rollback y manejo de conflictos en método POST Se solucionó un problema en el método POST que causaba un mal funcionamiento del rollback en caso de un envío duplicado en la clave primaria. Además, se realizaron cambios en la clase Conductor para adaptar los nombres de las propiedades a los métodos de búsqueda existentes en el repositorio. Esto incluyó la eliminación de los guiones bajos en las propiedades: - private String numeroChasis; - private String licenciaVigente; - private String TWICCard;
    Para habilitar la búsqueda por licencia vigente, TWIC card y número de chasis en el repositorio, se agregaron los métodos:
    - boolean existsByLicenciaVigente(String licenciaVigente);
    - boolean existsByTWICCard(String twicCard);
    - boolean existsByNumeroChasis(String numeroChasis);
    
    Además, se cambió el nombre de la columna TWIC_card a TWICCard en la tabla conductores para que los métodos de búsqueda funcionen correctamente.
    
    Se implementó un manejo de conflictos utilizando ResponseEntity para cada caso y se utilizó una lista para almacenar los errores, gestionados mediante un Stream

--- src/main/java/com/colaborativo/modulo_lyd/controller/ConductorController.java
+++ src/main/java/com/colaborativo/modulo_lyd/controller/ConductorController.java
@@ -5,34 +5,56 @@ import com.colaborativo.modulo_lyd.model.conductor.DatosActualizarConductor;
 import com.colaborativo.modulo_lyd.model.conductor.DatosListarConductor;
 import com.colaborativo.modulo_lyd.model.conductor.DatosRegistrarConductor;
 import com.colaborativo.modulo_lyd.repository.ConductorRepository;
-import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-    @RestController
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
     @RequestMapping("/conductor")
     public class ConductorController {
 
         @Autowired
         private ConductorRepository conductorRepository;
-
         @PostMapping
-        @Transactional
-        public void registrarConductor(@RequestBody @Valid DatosRegistrarConductor datosRegistrarConductor) {
-            conductorRepository.save(new Conductor(datosRegistrarConductor));
+        public ResponseEntity<?> registrarConductor(@RequestBody @Valid DatosRegistrarConductor datosRegistrarConductor) {
+
+            // Realiza todas las validaciones necesarias y las pongo en una lista usando Stream
+            List<String> errores = Stream.of(
+                            conductorRepository.existsByLicenciaVigente(datosRegistrarConductor.licencia_vigente()) //condicion
+                                    ? "Error: Ya existe un conductor con esta licencia vigente." // este se activa si la condicion es true o false
+                                    : null, // y este retorna esto si es false
+                            conductorRepository.existsByTWICCard(datosRegistrarConductor.twic_card())
+                                    ? "Error: Ya existe un conductor con este twic_card."
+                                    : null,
+                            conductorRepository.existsByNumeroChasis(datosRegistrarConductor.numero_chasis())
+                                    ? "Error: Ya existe un conductor con este Numero_chasis."
+                                    : null)
+                    .filter(error -> error != null).collect(Collectors.toList());  // filtra los elementos restantes diferentes de nulos y se lo agrega a una nueva lista
+
+            if (!errores.isEmpty()) { // si errores no esta vacia o si hay errores en la lista
+                return ResponseEntity.status(HttpStatus.CONFLICT).body(errores);
+            } else {
+                conductorRepository.save(new Conductor(datosRegistrarConductor));
+                return ResponseEntity.ok("Registro exitoso");
+            }
         }
 
         @GetMapping
-        public Page<DatosListarConductor> datosListarConductors(@PageableDefault(size = 10) Pageable paginacion) {
+        public Page<DatosListarConductor> datosListarConductors(@PageableDefault(size = 20) Pageable paginacion) {
             return conductorRepository.findByConductorVigenteTrue(paginacion).map(DatosListarConductor::new);
         }
 
         @PutMapping
-        @Transactional
+//        @Transactional
         public void actualizarConductor(@RequestBody @Valid DatosActualizarConductor datosActualizarConductor) {
             Conductor conductor = conductorRepository.getReferenceById(datosActualizarConductor.id());
             conductor.actualizarDatosConductor(datosActualizarConductor);
@@ -49,7 +71,7 @@ import org.springframework.web.bind.annotation.*;
 //        @DeleteMapping("/{id}")
 //        @Transactional
 //        public void eliminarConductor(@PathVariable Integer id) {
-//            // elimino al conductor sin dejar ningun registro de q exsistio
+//            // elimino al conductor sin dejar ningun registro en la db
 //            conductorRepository.deleteById(id);
 //        }
     }
--- src/main/java/com/colaborativo/modulo_lyd/model/conductor/DatosListarConductor.java
+++ src/main/java/com/colaborativo/modulo_lyd/model/conductor/DatosListarConductor.java
@@ -2,6 +2,6 @@ package com.colaborativo.modulo_lyd.model.conductor;
 
 public record DatosListarConductor(Integer Id_conductor, String nombre_conductor, String licencia_vigente, String TWIC_card, Long experiencia, Carga_peligrosa permiso_carga_peligrosa, String tipo_camion, Estado estado){
     public DatosListarConductor(Conductor conductor) {
-        this(conductor.getId_conductor(), conductor.getNombre_conductor(), conductor.getLicencia_vigente(), conductor.getTWIC_card(), conductor.getExperiencia(), conductor.getPermiso_carga_peligrosa(), conductor.getTipo_camion(), conductor.getEstado());
+        this(conductor.getId_conductor(), conductor.getNombre_conductor(), conductor.getLicenciaVigente(), conductor.getTWICCard(), conductor.getExperiencia(), conductor.getPermiso_carga_peligrosa(), conductor.getTipo_camion(), conductor.getEstado());
     }
 }
